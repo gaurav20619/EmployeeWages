21ddb21a84b30ef6b0e0b0c3a22b63be1cf016fb
gaurav20619 <gauravsinghraijada619@gmail.com>
2023-02-05 20:41:06 +0530

    Add: program to calculate daily employee wage.

--- src/main/java/com/bridgelabz/employeewages/EmployeeWages.java
+++ src/main/java/com/bridgelabz/employeewages/EmployeeWages.java
@@ -4,12 +4,19 @@ public class EmployeeWages {
     public static void main(String[] args) {
         //constants
         int isFullTime = 1;
+        int empRatePerHour = 20;
+        // variables
+        int empHrs = 0;
+        int empWage = 0;
         // computation
         int empCheck = (int)(Math.random()*2);
         if (empCheck==isFullTime) {
-            System.out.println("Employee is present");
+            empHrs = 8;
         } else {
-            System.out.println("Employee is absent");
+            empHrs = 0;
         }
+        empWage = empHrs * empRatePerHour;
+        System.out.println("Emp Wage is "+ empWage);
+        
     }
 }
