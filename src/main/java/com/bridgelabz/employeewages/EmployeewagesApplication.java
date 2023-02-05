805d05c15b6932629119f791af1c2a55cea9c666
gaurav20619 <gauravsinghraijada619@gmail.com>
2023-02-05 20:01:23 +0530

    Add: welcome message for employee wages program.

---
+++ src/main/java/com/bridgelabz/employeewages/EmployeewagesApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.employeewages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeewagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeewagesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridgelabz/employeewages/EmployeewagesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.employeewages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeewagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
