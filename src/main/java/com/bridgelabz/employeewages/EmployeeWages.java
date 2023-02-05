85f29521e77675b5a60ec9bcce3508735cd3a5bb
gaurav20619 <gauravsinghraijada619@gmail.com>
2023-02-05 23:15:10 +0530

    Add: program to calculate employee monthly wage.

--- src/main/java/com/bridgelabz/employeewages/EmployeeWages.java
+++ src/main/java/com/bridgelabz/employeewages/EmployeeWages.java
@@ -4,25 +4,32 @@ public class EmployeeWages {
     public static final int isPartTime = 1;
     public static final int isFullTime = 2;
     public static final int empRatePerHour = 20;
+    public static final int numOfWorkingDays = 20;
+    
     
     public static void main(String[] args) {
         // variables
         int empHrs = 0;
         int empWage = 0;
+        int totalEmployeeWage = 0;
         // computation
-        int empCheck = (int)(Math.random()*3);
-        switch (empCheck) {
-            case isPartTime:
-                empHrs = 4;
-                break;
-            case isFullTime:
-                empHrs = 8;
-                break;
-            default:
-                empHrs = 0;
-                break;
+        for (int day = 0; day<numOfWorkingDays; day++){
+            int empCheck = (int)(Math.random()*3);
+            switch (empCheck) {
+                case isPartTime:
+                    empHrs = 4;
+                    break;
+                case isFullTime:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+            }
+            empWage = empHrs * empRatePerHour;
+            totalEmployeeWage+=empWage;
+            System.out.println("Emp Wage is "+ empWage);
         }
-        empWage = empHrs * empRatePerHour;
-        System.out.println("Emp Wage is "+ empWage);
+        System.out.println("Total Emp Wage is "+ totalEmployeeWage);
     }
 }
